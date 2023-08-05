c784844586af2ee242d1395081a7ffca93e05e43
youssefezzahi youssefezzahi <elmrihy.mohamed.01@gmail.com>
2023-08-05 13:04:48 +0200

    deploy northflank

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-require("dotenv").config()
 const axios = require("axios")
 const app = express()
 app.use(cors())
